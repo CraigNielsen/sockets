ee8403b1fa807e662b62e802cf0c12a90629d33f
Craig Nielsen <craiglbn@gmail.com>
2017-05-06 14:03:20 +0200

    wip

--- client/client.py
+++ client/client.py
@@ -8,20 +8,15 @@ TCP_IP = '127.0.0.1'
 TCP_PORT = 5005
 BUFFER_SIZE = 1024
 MESSAGE = "Hello, World!"
-
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((TCP_IP, TCP_PORT))
 s.send(MESSAGE)
 data = s.recv(BUFFER_SIZE)
 x = 'in'
+s.send(MESSAGE)
 while x!='out':
-    s.send(MESSAGE)
     data = s.recv(BUFFER_SIZE)
     print("recieved response: {}".format(data))
-    s.send("a different message")
-    data = s.recv(BUFFER_SIZE)
-    print("recieved response: {}".format(data))
-    time.sleep(0.5)
 
 
 s.close()
--- server/server.py
+++ server/server.py
@@ -1,21 +1,69 @@
 #!/usr/bin/env python
 
 import socket
+import threading
+import time
+import random
+import math
 
+def should_send_many():
+    if random.random() > 0.2:
+        return True
+    else:
+        return False
 
-TCP_IP = '127.0.0.1'
-TCP_PORT = 5005
-BUFFER_SIZE = 20  # Normally 1024, but we want fast response
+def get_ship_message():
+    id_part=int(math.floor(random.random()*100000))
+    width=30
+    height=50
+    return '^{}|{}|{}^'.format(id_part,width,height)
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.bind((TCP_IP, TCP_PORT))
-s.listen(1)
+class ThreadedServer(object):
+    def __init__(self, host, port):
+        self.host = host
+        self.port = port
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        self.sock.bind((self.host, self.port))
 
-conn, addr = s.accept()
-print('Connection address:', addr)
-while 1:
-    data = conn.recv(BUFFER_SIZE)
-    if data:
-        print("received data:", data)
-    conn.send(data)  # echo
-conn.close()
+    def listen(self):
+        self.sock.listen(5)
+        while True:
+            client, address = self.sock.accept()
+            client.settimeout(60)
+            threading.Thread(target = self.listenToClient,args = (client,address)).start()
+
+    def listenToClient(self, client, address):
+        size = 1024
+        while True:
+            try:
+                data = client.recv(size)
+                if data:
+                    # Set the response to echo back the recieved data
+                    print("connection from: ", address)
+                    while True:
+                        response=get_ship_message()
+                        if should_send_many():
+                            for i in range(3):
+                                client.send(response)
+                                time.sleep(0.5)
+                        else:
+                            client.send(response)
+                            time.sleep(2)
+                else:
+                    raise Exception('Client disconnected')
+            except:
+                client.close()
+                return False
+
+if __name__ == "__main__":
+    while True:
+        port_num = 5005
+        # port_num = input("Port? ")
+        try:
+            port_num = int(port_num)
+            break
+        except ValueError:
+            pass
+
+    ThreadedServer('',port_num).listen()
